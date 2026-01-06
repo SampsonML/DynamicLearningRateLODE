ce1422de86782032d37d7661720ccb09c034eb22
Matt Sampson <matt.sampson@princeton.edu>
2026-01-05 21:29:43 -0500

    Split util functions

---
+++ src/dynamic_lode/utils/__init__.py
@@ -0,0 +1,2 @@
+from .hessian import top_hessian_eigenvalue
+from .schedules import make_schedule_fn, update_lr_buffer
--- src/dynamic_lode/utils/schedule.py
+++ src/dynamic_lode/utils/schedule.py
@@ -1,26 +1,6 @@
 from types import SimpleNamespace
 import jax.numpy as jnp
 import jax
-from jax.flatten_util import ravel_pytree
-
-def top_hessian_eigenvalue(loss_fn, params, data, num_iters=50, key=jax.random.PRNGKey(0)):
-    flat_params, unravel_fn = ravel_pytree(params)
-
-    def flat_loss(flat_params):
-        return loss_fn(unravel_fn(flat_params), data)
-
-    def hvp(v):
-        return jax.jvp(jax.grad(flat_loss), (flat_params,), (v,))[1]
-
-    vec = jax.random.normal(key, shape=flat_params.shape, dtype=flat_params.dtype)
-    vec = vec / (jnp.linalg.norm(vec) + 1e-8)
-
-    for _ in range(num_iters):
-        vec = hvp(vec)
-        vec = vec / (jnp.linalg.norm(vec) + 1e-8)
-
-    return jnp.dot(vec, hvp(vec))
-
 
 # A container to hold the lr_array (initial value)
 lr_holder = SimpleNamespace()
